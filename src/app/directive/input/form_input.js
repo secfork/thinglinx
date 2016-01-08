a7fbbe138740ef16590c0f58a467453f2d5bc763
grrey@163.com <grrey@163.com>
2016-01-09 00:10:27 +0800

    input 指令完善 Signed-off-by: grrey@163.com <grrey@163.com>

--- src/app/directive/input/form_input.js
+++ src/app/directive/input/form_input.js
@@ -33,27 +33,24 @@ export  default ( $compile  )=>{
 		restrict:"A", 
 		require: 'ngModel',
 		link:( scope , ele , attrs , modelCtrl )=>{
-			var  label = ' <label class= " col-sm-3 control-label " > '+ attrs.label +' </label> ' ,
-				 wrap_input = '<div class="form-group"><div class="col-sm-8"></div></div> ' ;
-
-
-	 
-
+			var label = ' <label class= " col-sm-3 control-label " > '+ attrs.label +' </label> ' ,
+				wrap_input = '<div class="form-group"><div class="col-sm-8"></div></div> ' ,
+ 				messageDom = $('<div></div>');
+				
  			scope.m = modelCtrl ; 
 
- 			var  messageDom = $('<div></div>');
 	       
-	        $attrs.required && 	messageDom.append(  pText('required') );
-	        $attrs.type  	&&  messageDom.append(  pText( $attrs.type ) );
-	        $attrs.max 		&&  messageDom.append( pText( 'max' ,  $attrs.max )  );
-	        $attrs.min 		&&  messageDom.append( pText( 'min' ,  $attrs.min ) );
-	        $attrs.ngMinlength  &&	messageDom.append( pText( 'minlength' ,  $attrs.ngMinlength  ) );
-	        $attrs.ngMaxlength 	&&  messageDom.append( pText( 'maxlength' ,  $attrs.ngMaxlength  ) );
+	        attrs.required  && 	messageDom.append(  pText('required') );
+	        attrs.type  	&&  messageDom.append(  pText( attrs.type ) );
+	        attrs.max 		&&  messageDom.append( pText( 'max' ,  attrs.max )  );
+	        attrs.min 		&&  messageDom.append( pText( 'min' ,  attrs.min ) );
+	        attrs.ngMinlength  &&	messageDom.append( pText( 'minlength' ,  attrs.ngMinlength  ) );
+	        attrs.ngMaxlength 	&&  messageDom.append( pText( 'maxlength' ,  attrs.ngMaxlength  ) );
 	       		
  
 	        ele.addClass(  ele.is("input , textarea ,select")?"form-control" : " no-border")
 				.wrap( wrap_input)
-	       	    .after( $compile( messageDom)( $scope) ) ;
+	       	    .after( $compile( messageDom)( scope) ) ;
 				.parent().before( label );
 
 
